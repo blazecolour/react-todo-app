eb04b34a91c97be54e21f14b676fc4ea04887327
blazecolour <936x936@gmail.com>
2018-11-21 15:37:41 +0300

    fix: fix index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,9 @@ import reducers from './reducers';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-
-
-/* eslint-disable no-underscore-dangle */
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
-/* eslint-enable */
-
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
